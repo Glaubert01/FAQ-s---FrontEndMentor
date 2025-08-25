f492aab4efebc35cc1b79c03e0be25c533457e76
Glaubert01 <glaubert@outlook.com>
2025-08-24 22:32:54 -0300

    clicar no paragrafo tambem funciona agora

--- app/script.js
+++ app/script.js
@@ -1,13 +1,20 @@
-const botao = document.querySelectorAll('.conteudo__icone-button');
-const paragrafo = document.querySelectorAll('.conteudo__item-paragrafo');
+const botoes = document.querySelectorAll('.conteudo__icone-button');
+const paragrafos = document.querySelectorAll('.conteudo__item-paragrafo');
+const perguntas = document.querySelectorAll('.conteudo__paragrafo'); // <-- título da pergunta
 
-botao.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        paragrafo[index].classList.toggle('hidden');
-        if(paragrafo[index].classList.contains('hidden')) {
-            btn.innerHTML = `<img class="conteudo__icone-img" src="assets/images/icon-plus.svg" alt="icone de mais">`
-        } else {
-            btn.innerHTML = `<img class="conteudo__icone-img" src="assets/images/icon-minus.svg" alt="icone de menos">`
-        }
-    });
+function toggleItem(index, btn) {
+    paragrafos[index].classList.toggle('hidden');
+    if (paragrafos[index].classList.contains('hidden')) {
+        btn.innerHTML = `<img class="conteudo__icone-img" src="assets/images/icon-plus.svg" alt="icone de mais">`
+    } else {
+        btn.innerHTML = `<img class="conteudo__icone-img" src="assets/images/icon-minus.svg" alt="icone de menos">`
+    }
+}
+
+botoes.forEach((btn, index) => {
+    btn.addEventListener('click', () => toggleItem(index, btn));
+});
+
+perguntas.forEach((pergunta, index) => {
+    pergunta.addEventListener('click', () => toggleItem(index, botoes[index]));
 });
